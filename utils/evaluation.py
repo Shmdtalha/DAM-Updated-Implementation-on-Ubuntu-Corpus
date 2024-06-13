5ef28884c79c810dd710ff5bbe6e6c28290c86d9
aleesha-w <132097373+aleesha-w@users.noreply.github.com>
2024-06-13 07:56:13 +0500

    Update evaluation.py

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -20,6 +20,8 @@ def get_p_at_n_in_m(data, n, m, ind):
 
 def evaluate(file_path):
     data = []
+    x=0
+    total_num=0
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
@@ -29,14 +31,20 @@ def evaluate(file_path):
                 continue
             
             data.append((float(tokens[0]), int(tokens[1])))
-    
+            if i % 10 == 9:
+                total_num += 1
+                total_num += 1
+                sum_m_a_p += m_a_p
+                m_a_p= mean_average_precision(data)
+            i += 1
+                
     p_at_1_in_2 = 0.0
     p_at_1_in_10 = 0.0
     p_at_2_in_10 = 0.0
     p_at_5_in_10 = 0.0
 
     length = int(len(data)/10)
-    MAP=mean_average_precision(data)
+    
     for i in range(0, length):
         ind = i * 10
         assert data[ind][1] == 1
@@ -46,4 +54,4 @@ def evaluate(file_path):
         p_at_2_in_10 += get_p_at_n_in_m(data, 2, 10, ind)
         p_at_5_in_10 += get_p_at_n_in_m(data, 5, 10, ind)
 
-    return (p_at_1_in_2/length, p_at_1_in_10/length, p_at_2_in_10/length, p_at_5_in_10/length, MAP)
+    return (p_at_1_in_2/length, p_at_1_in_10/length, p_at_2_in_10/length, p_at_5_in_10/length, 1.0*sum_m_a_p/total_num)

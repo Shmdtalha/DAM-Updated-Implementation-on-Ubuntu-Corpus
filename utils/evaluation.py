17375659df621eedf9da79550b7ee3d2a65a6f2a
aleesha-w <132097373+aleesha-w@users.noreply.github.com>
2024-06-13 07:47:49 +0500

    Update evaluation.py

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,5 +1,14 @@
 import sys
 
+def mean_average_precision(sort_data):
+    count_1 = 0
+    sum_precision = 0
+    for index in range(len(sort_data)):
+        if sort_data[index][1] == 1:
+            count_1 += 1
+            sum_precision += 1.0 * count_1 / (index+1)
+    return sum_precision / count_1
+
 def get_p_at_n_in_m(data, n, m, ind):
     pos_score = data[ind][0]
     curr = data[ind:ind+m]
@@ -27,7 +36,7 @@ def evaluate(file_path):
     p_at_5_in_10 = 0.0
 
     length = int(len(data)/10)
-
+    MAP=mean_average_precision(data)
     for i in range(0, length):
         ind = i * 10
         assert data[ind][1] == 1
@@ -37,4 +46,4 @@ def evaluate(file_path):
         p_at_2_in_10 += get_p_at_n_in_m(data, 2, 10, ind)
         p_at_5_in_10 += get_p_at_n_in_m(data, 5, 10, ind)
 
-    return (p_at_1_in_2/length, p_at_1_in_10/length, p_at_2_in_10/length, p_at_5_in_10/length)
+    return (p_at_1_in_2/length, p_at_1_in_10/length, p_at_2_in_10/length, p_at_5_in_10/length, MAP)

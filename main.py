05406b03ee0cf14eabdaaaadecf68ff88d09566b
Nafees Hassan <nafeeshassan16@gmail.com>
2024-08-05 21:04:18 +0500

    spaces -> tabs

--- main.py
+++ main.py
@@ -18,35 +18,35 @@ import bin.test_and_evaluate as test
 # configure
 
 conf = {
-    "data_path": "./data/ubuntu/data.pkl",
-    "save_path": "./output/ubuntu/temp/",
-    "word_emb_init": "./data/word_embedding.pkl",
-    "init_model": None, #should be set for test
+	"data_path": "./data/ubuntu/data.pkl",
+	"save_path": "./output/ubuntu/temp/",
+	"word_emb_init": "./data/word_embedding.pkl",
+	"init_model": None, #should be set for test
 
-    "rand_seed": None, 
+	"rand_seed": None,
 
-    "drop_dense": None,
-    "drop_attention": None,
+	"drop_dense": None,
+	"drop_attention": None,
 
-    "is_mask": True,
-    "is_layer_norm": True,
-    "is_positional": False,  
+	"is_mask": True,
+	"is_layer_norm": True,
+	"is_positional": False,
 
-    "stack_num": 5,  
-    "attention_type": "dot",
+	"stack_num": 5,
+	"attention_type": "dot",
 
-    "learning_rate": 1e-3,
-    "vocab_size": 434512,
-    "emb_size": 200,
-    "batch_size": 256, #200 for test
+	"learning_rate": 1e-3,
+	"vocab_size": 434512,
+	"emb_size": 200,
+	"batch_size": 256, #200 for test
 
-    "max_turn_num": 9,  
-    "max_turn_len": 50, 
+	"max_turn_num": 9,
+	"max_turn_len": 50,
 
-    "max_to_keep": 1,
-    "num_scan_data": 2,
-    "_EOS_": 28270, #1 for douban data
-    "final_n_class": 1,
+	"max_to_keep": 1,
+	"num_scan_data": 2,
+	"_EOS_": 28270, #1 for douban data
+	"final_n_class": 1,
 }
 
 
@@ -55,4 +55,3 @@ train.train(conf, model)
 
 #test and evaluation, init_model in conf should be set
 #test.test(conf, model)
-

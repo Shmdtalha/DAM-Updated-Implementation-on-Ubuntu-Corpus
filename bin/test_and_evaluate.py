cca47d9c82ff4a49eb7bebb7268610808dbc42b0
Nafees Hassan <nafeeshassan16@gmail.com>
2024-08-05 20:38:40 +0500

    Added time estimation to training

--- bin/test_and_evaluate.py
+++ bin/test_and_evaluate.py
@@ -11,77 +11,74 @@ import utils.evaluation as eva
 
 
 def test(conf, _model):
-    
-    if not os.path.exists(conf['save_path']):
-        os.makedirs(conf['save_path'])
 
-    # load data
-    print('starting loading data')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    train_data, val_data, test_data = pickle.load(open(conf["data_path"], 'rb'))    
-    print('finish loading data')
+	if not os.path.exists(conf['save_path']):
+		os.makedirs(conf['save_path'])
 
-    test_batches = reader.build_batches(test_data, conf)
+	# load data
+	print('starting loading data')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	train_data, val_data, test_data = pickle.load(open(conf["data_path"], 'rb'))
+	print('finish loading data')
 
-    print("finish building test batches")
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	test_batches = reader.build_batches(test_data, conf)
 
-    # refine conf
-    test_batch_num = len(test_batches["response"])
+	print("finish building test batches")
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    print('configurations: %s' %conf)
+	# refine conf
+	test_batch_num = len(test_batches["response"])
+
+	print('configurations: %s' %conf)
 
 
-    _graph = _model.build_graph()
-    print('build graph sucess')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	_graph = _model.build_graph()
+	print('build graph sucess')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    with tf.Session(graph=_graph) as sess:
-        #_model.init.run();
-        _model.saver.restore(sess, conf["init_model"])
-        print("sucess init %s" %conf["init_model"])
+	with tf.Session(graph=_graph) as sess:
+		#_model.init.run();
+		_model.saver.restore(sess, conf["init_model"])
+		print("sucess init %s" %conf["init_model"])
 
-        batch_index = 0
-        step = 0
+		batch_index = 0
+		step = 0
 
-        score_file_path = conf['save_path'] + 'score.test'
-        score_file = open(score_file_path, 'w')
+		score_file_path = conf['save_path'] + 'score.test'
+		score_file = open(score_file_path, 'w')
 
-        print('starting test')
-        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-        for batch_index in xrange(test_batch_num):
-                
-            feed = { 
-                _model.turns: test_batches["turns"][batch_index],
-                _model.tt_turns_len: test_batches["tt_turns_len"][batch_index],
-                _model.every_turn_len: test_batches["every_turn_len"][batch_index],
-                _model.response: test_batches["response"][batch_index],
-                _model.response_len: test_batches["response_len"][batch_index],
-                _model.label: test_batches["label"][batch_index]
-                }   
-                
-            scores = sess.run(_model.logits, feed_dict = feed)
-                    
-            for i in xrange(conf["batch_size"]):
-                score_file.write(
-                    str(scores[i]) + '\t' + 
-                    str(test_batches["label"][batch_index][i]) + '\n')
-                    #str(sum(test_batches["every_turn_len"][batch_index][i]) / test_batches['tt_turns_len'][batch_index][i]) + '\t' +
-                    #str(test_batches['tt_turns_len'][batch_index][i]) + '\n') 
+		print('starting test')
+		print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+		for batch_index in xrange(test_batch_num):
 
-        score_file.close()
-        print('finish test')
-        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+			feed = {
+				_model.turns: test_batches["turns"][batch_index],
+				_model.tt_turns_len: test_batches["tt_turns_len"][batch_index],
+				_model.every_turn_len: test_batches["every_turn_len"][batch_index],
+				_model.response: test_batches["response"][batch_index],
+				_model.response_len: test_batches["response_len"][batch_index],
+				_model.label: test_batches["label"][batch_index]
+				}
 
-        
-        #write evaluation result
-        result = eva.evaluate(score_file_path)
-        result_file_path = conf["save_path"] + "result.test"
-        with open(result_file_path, 'w') as out_file:
-            for p_at in result:
-                out_file.write(str(p_at) + '\n')
-        print('finish evaluation')
-        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-        
+			scores = sess.run(_model.logits, feed_dict = feed)
 
-                    
+			for i in xrange(conf["batch_size"]):
+				score_file.write(
+					str(scores[i]) + '\t' +
+					str(test_batches["label"][batch_index][i]) + '\n')
+					#str(sum(test_batches["every_turn_len"][batch_index][i]) / test_batches['tt_turns_len'][batch_index][i]) + '\t' +
+					#str(test_batches['tt_turns_len'][batch_index][i]) + '\n')
+
+		score_file.close()
+		print('finish test')
+		print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+
+		#write evaluation result
+		result = eva.evaluate(score_file_path)
+		result_file_path = conf["save_path"] + "result.test"
+		with open(result_file_path, 'w') as out_file:
+			for p_at in result:
+				out_file.write(str(p_at) + '\n')
+		print('finish evaluation')
+		print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
--- bin/train_and_evaluate.py
+++ bin/train_and_evaluate.py
@@ -11,126 +11,160 @@ import utils.evaluation as eva
 
 
 def train(conf, _model):
-    
-    if conf['rand_seed'] is not None:
-        np.random.seed(conf['rand_seed'])
 
-    if not os.path.exists(conf['save_path']):
-        os.makedirs(conf['save_path'])
+	if conf['rand_seed'] is not None:
+		np.random.seed(conf['rand_seed'])
 
-    # load data
-    print('starting loading data')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    train_data, val_data, test_data = pickle.load(open(conf["data_path"], 'rb'))    
-    print('finish loading data')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	if not os.path.exists(conf['save_path']):
+		os.makedirs(conf['save_path'])
 
-    val_batches = reader.build_batches(val_data, conf)
+	# load data
+	print('starting loading data')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	train_data, val_data, test_data = pickle.load(open(conf["data_path"], 'rb'))
+	print('finish loading data')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    print("finish building test batches")
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	val_batches = reader.build_batches(val_data, conf)
 
-    # refine conf
-    batch_num = int(len(train_data['y']) / conf["batch_size"])
-    val_batch_num = len(val_batches["response"])
+	print("finish building test batches")
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    conf["train_steps"] = conf["num_scan_data"] * batch_num
-    conf["save_step"] = int(max(1, batch_num / 10))
-    conf["print_step"] = int(max(1, batch_num / 100))
+	# refine conf
+	batch_num = int(len(train_data['y']) / conf["batch_size"])
+	val_batch_num = len(val_batches["response"])
 
-    print('configurations: %s' %conf)
+	conf["train_steps"] = conf["num_scan_data"] * batch_num
+	conf["save_step"] = int(max(1, batch_num / 10))
+	#conf["save_step"] = 10 # TODO remove this, only for debugging
+	conf["print_step"] = int(max(1, batch_num / 100))
 
-    print('model sucess')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	print('configurations: %s' %conf)
 
-    _graph = _model.build_graph()
-    print('build graph sucess')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+	print('model sucess')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-    with tf.Session(graph=_graph) as sess:
-        _model.init.run();
-        if conf["init_model"]:
-            _model.saver.restore(sess, conf["init_model"])
-            print("sucess init %s" %conf["init_model"])
+	_graph = _model.build_graph()
+	print('build graph sucess')
+	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-        average_loss = 0.0
-        batch_index = 0
-        step = 0
-        best_result = [0, 0, 0, 0]
+	with tf.Session(graph=_graph) as sess:
+		_model.init.run();
+		if conf["init_model"]:
+			_model.saver.restore(sess, conf["init_model"])
+			print("sucess init %s" %conf["init_model"])
 
-        for step_i in xrange(conf["num_scan_data"]):
-            #for batch_index in rng.permutation(range(batch_num)):
-            print('starting shuffle train data')
-            shuffle_train = reader.unison_shuffle(train_data)
-            train_batches = reader.build_batches(shuffle_train, conf)
-            print('finish building train data')
-            for batch_index in range(batch_num):
+		average_loss = 0.0
+		batch_index = 0
+		step = 0
+		best_result = [0, 0, 0, 0]
 
-                feed = {
-                    _model.turns: train_batches["turns"][batch_index], 
-                    _model.tt_turns_len: train_batches["tt_turns_len"][batch_index],
-                    _model.every_turn_len: train_batches["every_turn_len"][batch_index],
-                    _model.response: train_batches["response"][batch_index], 
-                    _model.response_len: train_batches["response_len"][batch_index],
-                    _model.label: train_batches["label"][batch_index]
-                }
+		steps_total = conf['num_scan_data'] * batch_num
+		saves_total = steps_total / conf['save_step']
+		saves = 0
+		print("Total steps: {} \t = {} * {}".format(steps_total, conf['num_scan_data'], batch_num))
+		print("Total saves: {} \t = {} / {}".format(saves_total, steps_total, conf['save_step']))
+		last_print_time = -1
+		last_save_time = -1
 
-                batch_index = (batch_index + 1) % batch_num;
+		for step_i in xrange(conf["num_scan_data"]):
+			#for batch_index in rng.permutation(range(batch_num)):
+			print('starting shuffle train data: {} / {}'.format(step_i, conf['num_scan_data']))
+			shuffle_train = reader.unison_shuffle(train_data)
+			train_batches = reader.build_batches(shuffle_train, conf)
+			print('finish building train data')
+			bt = time.time()
+			for batch_index in range(batch_num):
 
-                _, curr_loss = sess.run([_model.g_updates, _model.loss], feed_dict = feed)
+				feed = {
+					_model.turns: train_batches["turns"][batch_index],
+					_model.tt_turns_len: train_batches["tt_turns_len"][batch_index],
+					_model.every_turn_len: train_batches["every_turn_len"][batch_index],
+					_model.response: train_batches["response"][batch_index],
+					_model.response_len: train_batches["response_len"][batch_index],
+					_model.label: train_batches["label"][batch_index]
+				}
 
-                
-                average_loss += curr_loss
+				batch_index = (batch_index + 1) % batch_num;
 
-                step += 1
+				_, curr_loss = sess.run([_model.g_updates, _model.loss], feed_dict = feed)
 
-                if step % conf["print_step"] == 0 and step > 0:
-                    g_step, lr = sess.run([_model.global_step, _model.learning_rate])
-                    print('step: %s, lr: %s' %(g_step, lr))
-                    print("processed: [" + str(step * 1.0 / batch_num) + "] loss: [" + str(average_loss / conf["print_step"]) + "]" )
-                    average_loss = 0
 
-                
-                if step % conf["save_step"] == 0 and step > 0:
-                    index = step / conf['save_step']
-                    score_file_path = conf['save_path'] + 'score.' + str(index)
-                    score_file = open(score_file_path, 'w')
-                    print('save step: %s' %index)
-                    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+				average_loss += curr_loss
 
-                    for batch_index in xrange(val_batch_num):
-                
-                        feed = { 
-                            _model.turns: val_batches["turns"][batch_index],
-                            _model.tt_turns_len: val_batches["tt_turns_len"][batch_index],
-                            _model.every_turn_len: val_batches["every_turn_len"][batch_index],
-                            _model.response: val_batches["response"][batch_index],
-                            _model.response_len: val_batches["response_len"][batch_index],
-                            _model.label: val_batches["label"][batch_index]
-                        }   
-                
-                        scores = sess.run(_model.logits, feed_dict = feed)
-                    
-                        for i in xrange(conf["batch_size"]):
-                            score_file.write(
-                                str(scores[i]) + '\t' + 
-                                str(val_batches["label"][batch_index][i]) + '\n')
-                    score_file.close()
+				step += 1
 
-                    #write evaluation result
-                    result = eva.evaluate(score_file_path)
-                    result_file_path = conf["save_path"] + "result." + str(index)
-                    with open(result_file_path, 'w') as out_file:
-                        for p_at in result:
-                            out_file.write(str(p_at) + '\n')
-                    print('finish evaluation')
-                    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+				if step % conf["print_step"] == 0 and step > 0:
+					g_step, lr = sess.run([_model.global_step, _model.learning_rate])
 
-                    if result[1] + result[2] > best_result[1] + best_result[2]:
-                        best_result = result
-                        _save_path = _model.saver.save(sess, conf["save_path"] + "model.ckpt." + str(step / conf["save_step"]))
-                        print("succ saving model in " + _save_path)
-                        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-                    
-                
+					last_print_time = time.time() - bt
+					print('step: {} / {}, t = {}s'.format(
+						step, steps_total, last_print_time))
+					bt = time.time()
 
+					print('step: %s, lr: %s' %(g_step, lr))
+					print("processed: [" + str(step * 1.0 / batch_num) + "] loss: [" + str(average_loss / conf["print_step"]) + "]" )
+					average_loss = 0
+
+					if last_save_time != -1 and last_print_time != -1:
+						# estimate completion time
+						steps_left = steps_total - step
+						saves_left = saves_total - saves
+						eta = last_print_time * (steps_left / conf['print_step']) +\
+								last_save_time * saves_left
+						total = last_print_time * (steps_total / conf['print_step']) +\
+								last_save_time * saves_total
+						print("ETA: {}:{}:{} / {}:{}:{}".format(
+							int(eta / (60 * 60)),
+							int(eta / 60) % 60,
+							int(eta) % 60,
+							int(total / (60 * 60)),
+							int(total / 60) % 60,
+							int(total) % 60
+							))
+
+				if step % conf["save_step"] == 0 and step > 0:
+					st = time.time()
+					index = step / conf['save_step']
+					score_file_path = conf['save_path'] + 'score.' + str(index)
+					score_file = open(score_file_path, 'w')
+					print('save step: %s' %index)
+					print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+					for batch_index in xrange(val_batch_num):
+
+						feed = {
+							_model.turns: val_batches["turns"][batch_index],
+							_model.tt_turns_len: val_batches["tt_turns_len"][batch_index],
+							_model.every_turn_len: val_batches["every_turn_len"][batch_index],
+							_model.response: val_batches["response"][batch_index],
+							_model.response_len: val_batches["response_len"][batch_index],
+							_model.label: val_batches["label"][batch_index]
+						}
+
+						scores = sess.run(_model.logits, feed_dict = feed)
+
+						for i in xrange(conf["batch_size"]):
+							score_file.write(
+								str(scores[i]) + '\t' +
+								str(val_batches["label"][batch_index][i]) + '\n')
+					score_file.close()
+
+					#write evaluation result
+					result = eva.evaluate(score_file_path)
+					result_file_path = conf["save_path"] + "result." + str(index)
+					with open(result_file_path, 'w') as out_file:
+						for p_at in result:
+							out_file.write(str(p_at) + '\n')
+					print('finish evaluation')
+					print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+					if result[1] + result[2] > best_result[1] + best_result[2]:
+						best_result = result
+						_save_path = _model.saver.save(sess, conf["save_path"] + "model.ckpt." + str(step / conf["save_step"]))
+						print("succ saving model in " + _save_path)
+						print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+					last_save_time = time.time() - st
+					saves = saves + 1
+					print('save_step time: {}s'.format(last_save_time))
